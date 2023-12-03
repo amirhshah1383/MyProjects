c3b29b1c771d1955c9d33a744f472d9d548fe710
amirhshah1383 <152735153+amirhshah1383@users.noreply.github.com>
2023-12-03 12:07:18 -0800

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -2,29 +2,23 @@
 
 using namespace std;
 
-/* run this program using the console pauser or add your own getch, system("pause") or input loop */
-
-int main() {
-	float num1,num2,result;
-	char op;
-	cout << "please enter expression : ";
-	cin >> num1 >> op >> num2;
-	switch (op){
-		case '+':
-			result = num1 + num2;
-			break;
-		case '-':
-			result = num1 - num2;
-			break;
-		case '*':
-			result = num1 * num2;
-			break;
-		case '/':
-			result = num1 / num2;
-		    break;
-		case '%':
-			result = (int)num1 % (int)num2;
-	}
-	cout << "result = "<< result;
+ main() {
+ 	
+ 	float a,b,c;
+ 	
+ 	int x,y,z;
+ 	
+ 	cout << "please enter a number for x : ";
+ 	cin >> x;
+ 	cout << "please enter a number for y : ";
+    cin >> y;
+    cout << "please enter a number for z : ";
+    cin >> z;
+    
+    a = (x-y) * (x+z);
+    b =((x+y+z) * (x+y+z)) + 1;
+    c = a/b;
+    cout << "answer = "<<c;
+ 	
 	
 }

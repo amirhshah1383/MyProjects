59d85ccaff044f836553ef65a3df48538fe357d8
amirhshah1383 <152735153+amirhshah1383@users.noreply.github.com>
2023-12-08 22:03:51 +0330

    Add files via upload

---
+++ main4.cpp
@@ -0,0 +1,169 @@
+#include <iostream>
+
+using namespace std;
+
+ main() {
+ 	
+ 	char letter;
+ 	
+    cout << "please enter a letter : ";
+ 	cin >> letter;
+ 	
+ 	switch (letter){
+ 		case 'A':
+ 			cout << "VOWEL";
+ 			break;
+ 	    case 'a':
+ 	    	cout << "VOWEL";
+ 	    	break;
+ 	    case 'E':
+ 	    	cout << "VOWEL";
+ 	    	break;
+ 	    case 'e':
+ 	    	cout << "VOWEL";
+ 	    	break;
+ 	    case 'I':
+ 	    	cout << "VOWEL";
+ 	    	break;
+ 	    case 'i':
+ 	    	cout << "VOWEL";
+ 	    	break;
+ 	    case 'U':
+ 	    	cout << "VOWEL";
+ 	    	break;
+ 	    case 'u':
+ 	    	cout << "VOWEL";
+ 	    	break;
+ 	    case 'O':
+ 	    	cout << "VOWEL";
+ 	    	break;
+ 	    case 'o':
+ 	    	cout << "VOWEL";
+ 	    	break;
+ 	    case 'Y':
+ 	    	cout << "somtimes VOWEL and somtimes NO VOWEL";
+ 	    	break;
+ 	    case 'y':
+ 	    	cout << "somtimes VOWEL and somtimes NO VOWEL";
+ 	    	break;
+ 	    case 'B':
+ 	    	cout << "NO VOWEL";
+ 	    	break;
+ 	    case 'b':
+ 	    	cout << "NO VOWEL";
+ 	    	break;
+ 	    case 'C':
+ 	    	cout << "NO VOWEL";
+ 	    	break;
+ 	    case 'c':
+ 	    	cout << "NO VOWEL";
+ 	    	break;
+ 	    case 'D':
+ 	    	cout << "NO VOWEL";
+ 	    	break;
+ 	    case 'd':
+ 	    	cout << "NO VOWEL";
+ 	    	break;
+ 	    case 'F':
+ 	    	cout << "NO VOWEL";
+ 	    	break;
+ 	    case 'f':
+ 	    	cout << "NO VOWEL";
+ 	    	break;
+ 	    case 'G':
+ 	    	cout << "NO VOWEL";
+ 	    	break;
+ 	    case 'g':
+ 	    	cout << "NO VOWEL";
+ 	    	break;
+ 	    case 'H':
+ 	    	cout << "NO VOWEL";
+ 	    	break;
+ 	    case 'h':
+ 	    	cout << "NO VOWEL";
+ 	    	break;
+ 	    case 'J':
+ 	    	cout << "NO VOWEL";
+ 	    	break;
+ 	    case 'j':
+ 	    	cout << "NO VOWEL";
+ 	    	break;
+ 	    case 'K':
+ 	    	cout << "NO VOWEL";
+ 	    	break;
+ 	    case 'k':
+ 	    	cout << "NO VOWEL";
+ 	    	break;
+ 	    case 'L':
+ 	    	cout << "NO VOWEL";
+ 	    	break;
+ 	    case 'l':
+ 	    	cout << "NO VOWEL";
+ 	    	break;
+ 	    case 'M':
+ 	    	cout << "NO VOWEL";
+ 	    	break;
+ 	    case 'm':
+ 	    	cout << "NO VOWEL";
+ 	    	break;
+ 	    case 'N':
+ 	    	cout << "NO VOWEL";
+ 	    	break;
+ 	    case 'n':
+ 	    	cout << "NO VOWEL";
+ 	    	break;
+ 	    case 'P':
+ 	    	cout << "NO WOVEL";
+ 	    	break;
+ 	    case 'p':
+ 	    	cout << "NO VOWEL";
+ 	    	break;
+ 	    case 'Q':
+ 	    	cout << "NO VOWEL";
+ 	    	break;
+ 	    case 'q':
+ 	    	cout << "NO VOWEL";
+ 	    	break;
+ 	    case 'R':
+ 	    	cout << "NO VOWEL";
+ 	    	break;
+ 	    case 'r':
+ 	    	cout << "NO VOWEL";
+ 	    	break;
+ 	    case 'S':
+ 	    	cout << "NO VOWEL";
+ 	    	break;
+ 	    case 's':
+ 	    	cout << "NO VOWEL";
+ 	    	break;
+ 	    case 'T':
+ 	    	cout << "NO VOWEL";
+ 	    	break;
+ 	    case 't':
+ 	    	cout << "NO VOWEL";
+ 	    	break;
+ 	    case 'V':
+ 	    	cout << "NO VOWEL";
+ 	    	break;
+ 	    case 'v':
+ 	    	cout << "NO VOWEL";
+ 	    	break;
+ 	    case 'W':
+ 	    	cout << "NO VOWEL";
+ 	    	break;
+ 	    case 'w':
+ 	    	cout << "NO VOWEL";
+ 	    	break;
+ 	    case 'X':
+ 	    	cout << "NO VOWEL";
+ 	    	break;
+ 	    case 'x':
+ 	    	cout << "NO VOWEL";
+ 	    	break;
+ 	    case 'Z':
+ 	    	cout << "NO VOWEL";
+ 	    	break;
+ 	    case 'z':
+ 	    	cout << "NO VOWEL";
+	 }
+}
